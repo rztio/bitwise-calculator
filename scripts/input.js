c57d73001d568b2f0ff61d10386a5b74034d145a
shubot <rztio@users.noreply.github.com>
2017-11-30 20:30:49 +0000

    keyboard inputs

---
+++ scripts/input.js
@@ -0,0 +1,116 @@
+var bits = document.querySelectorAll(".bit_select");
+for (var i = 0; i < bits.length; i++) {
+  bits[i].addEventListener("click", function() {
+    changeBit(this.id);
+  });
+}
+
+var numbers = document.querySelectorAll(".number:not(#equal)");
+for (var i = 0; i < numbers.length; i++) {
+   numbers[i].addEventListener("click", function() {
+    setDisplay(this.id);
+  });
+}
+
+var operators = document.querySelectorAll(".operator:not(#back):not(#CE):not(#C), #equal");
+for (var i = 0; i < operators.length; i++) {
+   operators[i].addEventListener("click", function() {
+    setEquation(this.id);
+  });
+}
+
+back.addEventListener("click", function() {
+  backspace();
+});
+
+CE.addEventListener("click", function() {
+  clearLastEntry();
+});
+
+C.addEventListener("click", function() {
+  deleteNumber();
+  deleteEquation();
+  deleteBits();
+});
+
+
+window.addEventListener("keypress", function(e){
+  var charCode = (e.which) ? e.which : e.keyCode;
+  var charStr = String.fromCharCode(charCode);
+
+  switch(charStr){
+    case "1":
+      document.getElementById("one").click();
+      break;
+    case "2":
+      document.getElementById("two").click();
+      break;
+    case "3":
+      document.getElementById("three").click();
+      break;
+    case "4":
+      document.getElementById("four").click();
+      break;
+    case "5":
+      document.getElementById("five").click();
+      break;
+    case "6":
+      document.getElementById("six").click();
+      break;
+    case "7":
+      document.getElementById("seven").click();
+      break;
+    case "8":
+      document.getElementById("eight").click();
+      break;
+    case "9":
+      document.getElementById("nine").click();
+      break;
+    case "0":
+      document.getElementById("zero").click();
+      break;
+    case "<":
+      document.getElementById("zero_shiftl").click();
+      break;
+    case ">":
+      document.getElementById("signed_shiftr").click();
+      break;
+    case "/":
+      document.getElementById("zero_shiftr").click();
+      break;
+    case "&":
+      document.getElementById("and").click();
+      break;
+    case "|":
+      document.getElementById("or").click();
+      break;
+    case "^":
+      document.getElementById("xor").click();
+      break;
+    case "~":
+      document.getElementById("not").click();
+      break;
+    case "(":
+      document.getElementById("bracketo").click();
+      break;
+    case ")":
+      document.getElementById("bracketc").click();
+      break;
+    case "[":
+      document.getElementById("CE").click();
+      break;
+    case "]":
+      document.getElementById("C").click();
+      break;
+  }
+});
+
+window.addEventListener("keydown", function(e){
+  var charCode = (e.which) ? e.which : e.keyCode;
+  if (charCode === 13) {
+    e.preventDefault();
+    document.getElementById("equal").click();
+  } else if (charCode === 8) {
+    document.getElementById("back").click();
+  }
+});
---
+++ scripts/screen.js
@@ -0,0 +1,120 @@
+const NUMBER_LIMIT = 20;
+const EQUATION_LIMIT = 110;
+
+var equation = "";
+var number = "";
+var storeBits = "";
+
+require(["scripts/calculator"], function(parser){
+    //calculator.js now loaded
+})
+
+function backspace() {
+  number = number.slice(0, -1);
+  showNumber(number);
+}
+
+function clearLastEntry() {
+  if(number !== ""){
+     deleteNumber();
+  } else {
+    equation = equation.substring(0, equation.lastIndexOf(" ")-1);
+    showEquation(equation);
+  }
+}
+
+function deleteNumber() {
+  number = "";
+  showNumber(number);
+}
+
+function deleteEquation() {
+  equation = "";
+  showEquation(equation);
+}
+
+function deleteBits(){
+  storeBits = "";
+  for (var i = 0; i < bits.length; i++) {
+    document.getElementById(bits[i].id).innerHTML = "0";
+  };
+}
+
+function showNumber(n){
+    document.getElementById("display").innerHTML = n;
+}
+
+function showEquation(e){
+    document.getElementById("equation").innerHTML = e;
+}
+
+function changeBit(id) {
+  if (document.getElementById(id).innerHTML === "0"){
+      document.getElementById(id).innerHTML = "1";
+  } else {
+      document.getElementById(id).innerHTML = "0";
+  }
+
+  for (var i = 0; i < bits.length; i++) {
+    storeBits += document.getElementById(bits[i].id).innerHTML;
+  }
+  number = bigInt(storeBits, 2);
+  showNumber(number);
+  storeBits = "";
+}
+
+function setDisplay(id){
+  if (number.length > NUMBER_LIMIT) return;
+  number += document.getElementById(id).innerHTML;
+  showNumber(number);
+}
+
+function setEquation(id){
+  if (storeBits === "") deleteBits();
+
+  if (id !== "equal") {
+    if (equation.length < EQUATION_LIMIT) {
+      includeNumber();
+      equation += convertOperator(id);
+    }
+  } else if (id === "equal"){
+    includeNumber();
+    try {
+      var result = parser.parse(input);
+      showNumber(result);
+    } catch (err) {
+      showNumber(err.messages);
+    }
+  }
+  showEquation(equation);
+}
+
+function includeNumber(){
+  if (number !== ""){
+    equation = equation + " " + number;
+    number = "";
+    showNumber(number);
+  }
+}
+
+function convertOperator(id){
+  var op = "";
+
+  switch (id) {
+    case "zero_shiftl":
+      op = " <<";
+      break;
+    case "signed_shiftr":
+      op = " >>";
+      break;
+    case "zero_shiftr":
+      op = " >>>";
+      break;
+    case "and":
+      op = " &";
+      break;
+    default:
+      op = document.getElementById(id).innerHTML;
+    }
+  return op;
+}
