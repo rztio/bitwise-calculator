ad897315c4f4bb754e84aa84e17969758f0bad79
shubot <rztio@users.noreply.github.com>
2017-11-30 20:36:30 +0000

    input

--- scripts/input.js
+++ scripts/input.js
@@ -107,10 +107,11 @@ window.addEventListener("keypress", function(e){
 
 window.addEventListener("keydown", function(e){
   var charCode = (e.which) ? e.which : e.keyCode;
-  e.preventDefault();
   if (charCode === 13) {
+    e.preventDefault();
     document.getElementById("equal").click();
   } else if (charCode === 8) {
+    e.preventDefault();
     document.getElementById("back").click();
   }
 });
